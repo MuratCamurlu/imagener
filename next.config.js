989ea1d80ae31e41c42be4a4cfac0bc197bf1b8e
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-04-06 15:14:27 +0300

    config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "replicate.delivery",
+        protocol: "https",
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
